7e529198a7ae6397e63c6c1db5021c15559b449d
Md Nasir Ibne Hussain <42325538+mnihussain@users.noreply.github.com>
2018-08-27 13:18:48 -0400

    Update Data.java

--- DataStru_sample/src/ds/Data.java
+++ DataStru_sample/src/ds/Data.java
@@ -2,6 +2,11 @@ package ds;
 
 public class Data {
     public static void main(String[] args) {
+        Data.MonDay();
         System.out.println("..");
+       
     }
+    
+    public static void MonDay(){
+        System.out.println("Came at 11:30 am);
 }
